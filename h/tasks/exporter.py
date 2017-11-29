621af5e4e3f6936afc46d0117cef89273a6241d0
m1yag1 <mike.arbelaez@rice.edu>
2017-11-28 22:20:32 -0600

    updated new column names for annotation export

--- h/tasks/exporter.py
+++ h/tasks/exporter.py
@@ -7,6 +7,8 @@ from boxsdk import JWTAuth, Client
 from celery.utils.log import get_task_logger
 from datetime import datetime
 
+from sqlalchemy import func
+
 from h import models, settings
 from h.celery import celery
 
@@ -57,22 +59,49 @@ def export_annotations():
         client_secret=celery.request.registry.settings['h.box_client_secret'],
         enterprise_id=celery.request.registry.settings['h.box_enterprise_id'],
         jwt_key_id=celery.request.registry.settings['h.box_jwt_key_id'],
-        rsa_private_key_passphrase=celery.request.registry.settings['h.box_rsa_private_key_pass'],
-        rsa_private_key_file_sys_path=celery.request.registry.settings['h.box_rsa_private_key_path'],
+        rsa_private_key_passphrase=celery.request.registry.settings[
+            'h.box_rsa_private_key_pass'],
+        rsa_private_key_file_sys_path=celery.request.registry.settings[
+            'h.box_rsa_private_key_path'],
         store_tokens=store_tokens
     )
 
     access_token = auth.authenticate_instance()
     client = Client(auth)
+    annotation = models.Annotation
+    app_url = celery.request.registry.settings['h.app_url']
 
-    annotations = celery.request.db.query(models.Annotation).all()
+    annotations = celery.request.db.query(
+        annotation.id.label('annotation_id'),
+        annotation.created.label('created_on'),
+        annotation.updated.label('updated_on'),
+        annotation.deleted.label('is_deleted'),
+        annotation.document_id,
+        annotation.tags,
+        func.regexp_replace(annotation.userid, '[^0-9]*', '', 'g').label(
+            'user_id'),
+        annotation._text.label('annotated_text'),
+        annotation.target_selectors[0]['content'].label('highlighted_text'),
+        annotation._target_uri.label('module_id')
+    ).distinct(annotation.id).all()
+
+    attrs = [
+        'annotation_id',
+        'created_on',
+        'updated_on',
+        'is_deleted',
+        'document_id',
+        'tags',
+        'user_id',
+        'annotated_text',
+        'highlighted_text',
+        'module_id'
+    ]
 
     if annotations:
+        file_name = datetime.now().strftime(app_url + '-%Y%m%d.csv')
 
-        file_name = datetime.now().strftime('hypothesis' + '-%Y%m%d.csv')
+        csv_file = to_csv(attrs, annotations)
 
-        field_names = get_field_names(annotations[0])
-
-        csv_file = to_csv(field_names, annotations)
-
-        client.folder(folder_id=box_folder_id).upload_stream(csv_file, file_name)
+        client.folder(folder_id=box_folder_id).upload_stream(csv_file,
+                                                             file_name)
